d03b23230485ab4dac2050c8c7d99bee8ed8d236
DanduNandini <165766420+DanduNandini@users.noreply.github.com>
2025-09-11 12:07:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Length of Last word/length-of-last-word.java
@@ -0,0 +1,19 @@
+// User function Template for Java
+class Solution {
+    int findLength(String s) {
+        int length = 0;
+        int i = s.length() - 1;
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+
+    
+
+    }
