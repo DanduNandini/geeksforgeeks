769360f285902a3c4173ed843e8433d798442d5c
DanduNandini <165766420+DanduNandini@users.noreply.github.com>
2025-09-19 11:55:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse first K of a Queue/reverse-first-k-of-a-queue.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public Queue<Integer> reverseFirstK(Queue<Integer> q, int k) {
+        if (q == null || k <= 0 || k > q.size()) {
+            return q;
+        }
+
+        int n = q.size();
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = q.poll();
+        }
+        for (int i = k - 1; i >= 0; i--) {
+            q.offer(res[i]);
+        }
+        for (int i = 0; i < n - k; i++) {
+            q.offer(q.poll());
+        }
+
+        return q;
+    }
+}
+
+
+
+        
