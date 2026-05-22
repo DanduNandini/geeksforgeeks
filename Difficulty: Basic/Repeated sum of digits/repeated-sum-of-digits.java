643d88a780c7412df4b2f7bb380be4a067f903c8
DanduNandini <165766420+DanduNandini@users.noreply.github.com>
2026-05-22 19:10:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Repeated sum of digits/repeated-sum-of-digits.java
@@ -0,0 +1,17 @@
+// User function Template for Java
+class Solution {
+    static int repeatedSumOfDigits(int num) {
+        // code here
+        while(num>9){
+             int sum=0;
+        while(num>0){
+            int digit =num %10;
+            sum=sum+ digit;
+            num=num/10;
+        }
+        num=sum;
+        }
+        return num;
+
+    }
+}
